cf6ce4ab3f47d55571cb11f564fc8e331ac80449
Cafelatte1 <57206771+Cafelatte1@users.noreply.github.com>
2024-01-31 14:59:02 +0900

    add_max_length_for_gptapi

--- HuggingRAG/chatgpt_vector_embedding.py
+++ HuggingRAG/chatgpt_vector_embedding.py
@@ -7,14 +7,15 @@ from openai import OpenAI
 """## Create Vector Embedding"""
     
 class ChatGPTVectorEmbedding():
-    def __init__(self, model_id):
+    def __init__(self, model_id, max_len):
         self.vectors = None
         self.store = None
         self.model_id = model_id
+        self.max_len = max_len
         self.client = OpenAI()
 
     def get_vectorembedding(self, docs, norm=True):
         docs = [docs] if isinstance(docs, str) else docs        
-        embed = torch.from_numpy(np.array([self.client.embeddings.create(input=[doc], model=self.model).data[0].embedding for doc in docs], dtype='float32'))
+        embed = torch.from_numpy(np.array([self.client.embeddings.create(input=[doc[:self.max_len]], model=self.model).data[0].embedding for doc in docs], dtype='float32'))
         embed = F.normalize(embed, p=2, dim=1).detach().cpu().numpy() if norm else embed.detach().cpu().numpy()
         return embed
