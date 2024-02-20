d801c48159318ea20433f351952c4bb248644113
Cafelatte1 <flash659@gmail.com>
2024-02-20 20:21:46 +0900

    update

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="HuggingRAG",
     version="0.1.0",
     description="High-Level Library for RAG task with HuggingFace API",
-    url="https://github.com/Cafelatte1/hugging-rag.git",
+    url="https://github.com/Cafelatte1/hugging-rag-scann.git",
     author="Cafelatte1",
     license="MIT",
     python_requires='>=3.10',
